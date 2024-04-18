8e712db885156ace3f14b31d27c1060867cf7233
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-04-17 22:08:44 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+ 
+ const closeButton = document.querySelector('.close-nav');
+ const openButton = document.querySelector('.open-nav');
+ const nav = document.querySelector('.nav');
+
+
+ openButton.addEventListener("click", () => {
+    nav.classList.add('navigation-open');
+}); 
+
+
+closeButton.addEventListener("click", () => {
+     nav.classList.remove('navigation-open');
+}); 
+
